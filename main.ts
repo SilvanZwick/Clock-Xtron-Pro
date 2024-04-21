c327261d2ec117cec7d9f5f37c7f1ea8bba3733c
Silvan <119003429+SilvanZwick@users.noreply.github.com>
2024-04-21 12:04:23 -0700

    Added start splash and made necessary variables

--- main.ts
+++ main.ts
@@ -1,1 +1,9 @@
-
+let Time = ""
+game.splash("Welcome!", "...to my clock app.")
+let Hours = 0
+forever(function () {
+    let Milliseconds = 0
+    let Seconds = 0
+    let Minutes = 0
+    Time = "" + Hours + ":" + Minutes + ":" + Seconds + "." + Milliseconds
+})
